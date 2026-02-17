7efb9cf669e8caa6244f42aeb02c45f006642d6f
gauthierauge <gauthier.auge@2dcom.fr>
2026-02-17 16:38:40 +0100

    fix(prisma.config.ts): update tsx errors

--- back/src/config/db.ts
+++ back/src/config/db.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from "../generated/prisma/client.js"
 import { DATABASE_URL } from "./env.js"
 
 const prisma = new PrismaClient({
-  datasourceUrl: DATABASE_URL,
+  accelerateUrl: DATABASE_URL,
 })
 
 export { prisma }
