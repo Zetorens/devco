580b208415bbd8e93735fc0f1d3092e7688d97df
gauthierauge <gauthier.auge@2dcom.fr>
2026-02-17 16:29:31 +0100

    feature/prisma: add prisma config

--- back/src/config/env.ts
+++ back/src/config/env.ts
@@ -1,4 +1,4 @@
-const PORT = process.env.BACK_PORT || 5000;
+const PORT = process.env.PORT || 3001;
 const DATABASE_URL = process.env.DATABASE_URL || "";
 const FRONT_URL = process.env.FRONT_URL || "http://localhost:3000";
 
