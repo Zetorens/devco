b86b15452c38c469beece2a70ff98c02d7fb32d7
gauthierauge <gauthier.auge@2dcom.fr>
2026-02-17 16:19:33 +0100

    feature/config: fix imports tsx errors

--- back/src/app.ts
+++ back/src/app.ts
@@ -1,9 +1,9 @@
 import express from "express"
 import helmet from "helmet"
 import cors from "cors"
-import { corsOptions } from "./cors.js"
-import { helmetOptions } from "./helmet.js"
-import {initRoutes} from "./routes.js";
+import { corsOptions } from "./config/cors.js"
+import { helmetOptions } from "./config/helmet.js"
+import {initRoutes} from "./config/routes.js";
 
 const createApp = () => {
     const app = express()
