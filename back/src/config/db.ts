48fb1fbf6471afc23f9561d0dc784693a706852e
gauthierauge <gauthier.auge@2dcom.fr>
2026-02-17 16:48:10 +0100

    fix(prisma + postgres): correct errors connection between prisma / postgres
    fix(prisma + postgres): correct errors connection between prisma / postgres

--- back/src/config/db.ts
+++ back/src/config/db.ts
@@ -1,8 +1,8 @@
 import { PrismaClient } from "../generated/prisma/client.js"
+import { PrismaPg } from "@prisma/adapter-pg"
 import { DATABASE_URL } from "./env.js"
 
-const prisma = new PrismaClient({
-  accelerateUrl: DATABASE_URL,
-})
+const adapter = new PrismaPg({ connectionString: DATABASE_URL })
+const prisma = new PrismaClient({ adapter })
 
 export { prisma }
