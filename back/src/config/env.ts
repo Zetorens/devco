04e349bd7ce8424465ea0a2cb324cd31f1b2b153
gauthierauge <gauthier.auge@2dcom.fr>
2026-02-18 12:19:28 +0100

    feat(CORS): update CORS restrictions

--- back/src/config/env.ts
+++ back/src/config/env.ts
@@ -1,5 +1,5 @@
 const PORT = process.env.PORT || 3001;
 const DATABASE_URL = process.env.DATABASE_URL || "";
-const FRONT_URL = process.env.FRONT_URL || "http://localhost:3000";
+const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:3000";
 
-export { PORT, DATABASE_URL, FRONT_URL };
+export { PORT, DATABASE_URL, FRONTEND_URL };
