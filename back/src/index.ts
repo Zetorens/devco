00dcc25904423b804f533fed25033ea205ba32c4
gauthierauge <gauthier.auge@2dcom.fr>
2026-02-18 12:42:08 +0100

    fix(env): correct backend url

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,7 +1,7 @@
 
-import { PORT } from "./config/env.js";
+import { BACKEND_PORT } from "./config/env.js";
 import {createApp} from "./app.js";
 
 const app = createApp();
 
-app.listen(PORT, () => console.log(`🚀 API running on http://localhost:${PORT}`));
+app.listen(BACKEND_PORT, () => console.log(`🚀 API running on http://localhost:${BACKEND_PORT}`));
