de278aa62abdd8fabe6b39729fbf6ac965093930
gauthierauge <gauthier.auge@2dcom.fr>
2026-02-18 12:42:08 +0100

    fix(env): correct backend url

--- back/src/config/env.ts
+++ back/src/config/env.ts
@@ -1,5 +1,5 @@
-const PORT = process.env.PORT || 3001;
+const BACKEND_PORT = process.env.BACKEND_PORT || 5000;
 const DATABASE_URL = process.env.DATABASE_URL || "";
 const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:3000";
 
-export { PORT, DATABASE_URL, FRONTEND_URL };
+export { BACKEND_PORT, DATABASE_URL, FRONTEND_URL };
