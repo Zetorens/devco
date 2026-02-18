2038e3b0d72ec2a9cc49f63c2001f6fa29d71581
gauthierauge <gauthier.auge@2dcom.fr>
2026-02-18 13:00:16 +0100

    refactor(env): replace BACKEND_PORT with BACKEND_URL as single source of truth

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,7 +1,7 @@
 
-import { BACKEND_PORT } from "./config/env.js";
+import { BACKEND_URL, BACKEND_PORT } from "./config/env.js";
 import {createApp} from "./app.js";
 
 const app = createApp();
 
-app.listen(BACKEND_PORT, () => console.log(`🚀 API running on http://localhost:${BACKEND_PORT}`));
+app.listen(BACKEND_PORT, () => console.log(`🚀 API running on ${BACKEND_URL}`));
